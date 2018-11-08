cd0dcb15c48328af50f4180141c76ee01518d85b
PETR RODIONOV <waserman6@gmail.com>
2018-11-08 14:07:10 +1000

    add unity project

---
+++ Assets/scroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroll : MonoBehaviour {
+
+    public float scrollSpeed = -5f;
+    Vector2 startPos;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed, 10);
+        transform.position = startPos + Vector2.down * newPos;
+	}
+}
