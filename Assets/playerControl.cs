0a7582032b7b0fcba0079a9ccb4af38d531f0500
PETR RODIONOV <waserman6@gmail.com>
2018-11-08 16:05:05 +1000

    Add joystick

--- Assets/playerControl.cs
+++ Assets/playerControl.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerControl : MonoBehaviour {
+    public float speed = 2f;
+    public Joystick joystick;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update() {
+     
+        transform.position = transform.position + new Vector3 (joystick.Direction.x * speed, joystick.Direction.y * speed, 0);
+    }
 }
