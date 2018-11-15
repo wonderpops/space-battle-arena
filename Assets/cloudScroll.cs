f72747c781f13c00a074fb9ef763b5077afe0051
PETR RODIONOV <waserman6@gmail.com>
2018-11-15 14:52:25 +1000

    Add Menu Scene

---
+++ Assets/cloudScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudScroll : MonoBehaviour {
+
+    public float scrollSpeed = -5f;
+    Vector2 startPos;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollSpeed , 20);
+        transform.position = startPos + Vector2.right * newPos;
+    }
+}
---
+++ Assets/startButtonPress.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class startButtonPress : MonoBehaviour {
+
+    private Button btn;
+
+    private Animator anim;
+    public Animator animbtn;
+    public Animator animShip;
+    
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        anim.enabled = true;
+        animbtn.enabled = true;
+        animShip.SetInteger("State", 3);
+        //animShip.SetInteger("State", 1);
+        //       System.Timers.Timer aTimer = new System.Timers.Timer();
+        //       aTimer.Elapsed+=new ElapsedEventHandler(OnTimedEvent);
+        //       aTimer.Interval=1000;
+        //       aTimer.Enabled=true;
+        //}
+
+        //   public void OnTimedEvent(object source, ElapsedEventArgs e){
+        //       animShip.SetInteger("State", 2);
+    }
+}
