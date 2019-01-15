d01e39161a401e7fdce3493bcdccd07a72258df8
CHACHACHACHACh <vladok_vip@mail.ru>
2019-01-16 00:23:06 +1000

    Add game area bound

--- Assets/SpaceShipControl.cs
+++ Assets/SpaceShipControl.cs
@@ -11,9 +11,10 @@ public class SpaceShipControl : Photon.MonoBehaviour
     Rigidbody2D r2d;
     public  GameObject bullet;
     public GameObject PointBullet;
+   // public GameObject autobullet;
     float fireRate,nextFire;
     public Image hpBar;
-
+    private Vector2 halfScreen;
     private Vector3 realpos = Vector3.zero;
     float TimeShoot;
 
@@ -30,12 +31,40 @@ public class SpaceShipControl : Photon.MonoBehaviour
         r2d = GetComponent<Rigidbody2D>();
         fireRate = 2f;
         nextFire = Time.time;
-
         playerStats.hp = 10;
         playerStats.name = GetComponent<PhotonView>().viewID.ToString();
         gameObject.name = playerStats.name;
+        halfScreen = Camera.main.ViewportToWorldPoint(new Vector2(0, 0.5f));
+
     }
-    
+    void Control(Touch touch, Vector2 touchPos)
+    {
+        
+        switch (touch.phase)
+        {
+            case TouchPhase.Began:
+                deltaX = touchPos.x - +transform.position.x;
+                deltaY = touchPos.y - +transform.position.y;
+                break;
+            case TouchPhase.Moved:
+                //if (r2d.transform.position.y > 1)
+                //{
+                //    r2d.MovePosition(new Vector2(touchPos.x - deltaX, 1));
+                //    //r2d.velocity = Vector2.zero;
+
+                //}
+                //else
+                //{
+
+                r2d.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
+                //}
+
+                break;
+            case TouchPhase.Ended:
+                r2d.velocity = Vector2.zero;
+                break;
+        }
+    }
     void Update()
     {
         //your ship cheker
@@ -45,31 +74,91 @@ public class SpaceShipControl : Photon.MonoBehaviour
             {
                 Touch touch = Input.GetTouch(0);
                 Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-                switch (touch.phase)
+                Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+                Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+                
+                if (transform.position.x > max.x)
                 {
-                    case TouchPhase.Began:
-                        deltaX = touchPos.x - +transform.position.x;
-                        deltaY = touchPos.y - +transform.position.y;
-                        break;
-                    case TouchPhase.Moved:
-                        r2d.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
-                        break;
-                    case TouchPhase.Ended:
-                        r2d.velocity = Vector2.zero;
-                        break;
+                    transform.SetPositionAndRotation(new Vector2(max.x,transform.position.y), transform.rotation);
+                } else
+                {
+                    Control(touch, touchPos);
+                }
+                if (transform.position.x < min.x){
+                    transform.SetPositionAndRotation(new Vector2(min.x, transform.position.y), transform.rotation);
+                }
+                else
+                {
+                    Control(touch, touchPos);
+                }
+                if (transform.position.y > max.y)
+                {
+                    transform.SetPositionAndRotation(new Vector2(transform.position.x, max.y), transform.rotation);
+                }
+                else
+                {
+                    Control(touch, touchPos);
+                }
+                if (transform.position.y < min.y)
+                {
+                    transform.SetPositionAndRotation(new Vector2(transform.position.x, min.y), transform.rotation);
+                }
+                else 
+                {
+                    Control(touch, touchPos);
                 }
 
 
-            }
+                if ((transform.position.x > max.x) && (transform.position.y < min.y))
+                {
+                    transform.SetPositionAndRotation(new Vector2(max.x, min.y), transform.rotation);
+                }
+                else
+                {
+                    Control(touch, touchPos);
+                }
+                if ((transform.position.x < min.x) && (transform.position.y < min.y))
+                {
+                    transform.SetPositionAndRotation(new Vector2(min.x, min.y), transform.rotation);
+                }
+                else
+                {
+                    Control(touch, touchPos);
+                }
+                if ((transform.position.y > max.y) && (transform.position.x > max.x))
+                {
+                    transform.SetPositionAndRotation(new Vector2(max.x, max.y), transform.rotation);
+                }
+                else
+                {
+                    Control(touch, touchPos);
+                }
+                if ((transform.position.y > max.y) && (transform.position.x < min.x))
+                {
+                    transform.SetPositionAndRotation(new Vector2(min.x, max.y), transform.rotation);
+                }
+                else
+                {
+                    Control(touch, touchPos);
+                }
+                
 
+            }
+          
         }
         else
         {
-            Sync();   
+            Sync();
+            
         }
 
-        if (Time.time > nextFire) 
+        if (Time.time > nextFire)
+        {
+
             Shoot();
+           // Shoot1();
+        }
+      
     }
 
     //streams
@@ -99,4 +188,10 @@ public class SpaceShipControl : Photon.MonoBehaviour
         Instantiate(bullet, PointBullet.transform.position, PointBullet.transform.rotation);
         nextFire = Time.time + fireRate;        
     }
+    //auto shoot
+    // void Shoot1()
+    //{
+    //    Instantiate(autobullet, PointBullet.transform.position, PointBullet.transform.rotation);
+    //    nextFire = Time.time + fireRate;
+    //}
 }
---
+++ Assets/autobullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autobullet : MonoBehaviour {
+    public int damage;
+    Vector2 move;
+    Rigidbody2D rb;
+    PhotonView target;
+    PhotonPlayer thisShip;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+
+        //move = (target.transform.position - transform.position).normalized * 7f;
+        //rb.velocity = new Vector2(move.x, move.y);
+       // Destroy(gameObject, 3f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        target = GetComponent<PhotonView>();
+        Debug.Log(target.name);
+
+    }
+}
